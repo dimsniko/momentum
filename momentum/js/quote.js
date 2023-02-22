2a6b5ef2cf1b09de92e4f962a32fe5ef913f6578
dimsniko <dimsniko@gmail.com>
2023-02-22 20:06:49 +0400

    data, greeting, slider, weather, quotes ready

--- momentum/js/quote.js
+++ momentum/js/quote.js
@@ -3,28 +3,35 @@ const quote = document.querySelector('.quote')
 const author = document.querySelector('.author')
 const changeQuote = document.querySelector('.change-quote') 
 
-const getRandomNum = (min, max) => {
+async function getRandomNum(min, max) {
     min = 0
     max = 30
     return Math.floor(Math.random() * (max-min + 1)) + min
 }
 
-
-
 async function getQuotes() {  
-    let randomNum = getRandomNum()
-    const quotes = 'data.json';
-    const res = await fetch(quotes);
+  let beforeNumber = localStorage.getItem('beforeNumber');
+  console.log(`oldValue ${beforeNumber}`)
+    let randomNum = await getRandomNum()
+    const res = await fetch('data.json');
     const data = await res.json(); 
-    quote.textContent = data[randomNum].text    
-    author.textContent = data[randomNum].author
-    console.log(data);
-    
+
+    console.log(`randomValue ${randomNum}`)
+
+   while (beforeNumber == randomNum) {
+    console.log("Enter while cycle")
+      randomNum = await getRandomNum()
+    }
+
+beforeNumber = randomNum;
+console.log(`newRandomNumber: ${randomNum}`)
+
+localStorage.setItem('beforeNumber', beforeNumber);
+
+    quote.textContent = data[randomNum].text;   
+    author.textContent = data[randomNum].author;
   }
-  getQuotes();
 
-
-  
+ getQuotes();
   
 changeQuote.addEventListener('click', getQuotes)
-console.dir(quote)
